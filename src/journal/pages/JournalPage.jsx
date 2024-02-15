35b560b9f4e007edb28de3518cd6a7765f4d7710
Emmanuel Barreto <barretosantiago@hotmail.com>
2024-02-15 19:48:41 -0300

    maquetado terminado

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,5 +1,32 @@
-import { Typography } from "@mui/material";
+import { AddOutlined } from "@mui/icons-material";
+import { IconButton, Typography } from "@mui/material";
+import { JournalLayout } from "../layout/JournalLayout";
+import { NoteView, NothingSelectedView } from "../views";
 
 export const JournalPage = () => {
-  return <Typography>JournalPage</Typography>;
+  return (
+    <JournalLayout>
+      {/* <Typography>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+        cupiditate velit amet aliquid fugiat doloribus nihil atque, rem hic
+        dolor consequatur vitae ad autem fugit minima eius ipsa eos sed?
+      </Typography> */}
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+
+      <IconButton
+        size="large"
+        sx={{
+          color: "white",
+          backgroundColor: "error.main",
+          ":hover": { backgroundColor: "error.main", opacity: 0.8 },
+          position: "fixed",
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
+    </JournalLayout>
+  );
 };
